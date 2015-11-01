586cbbaededa5b3eeb906f757ca8d363d22153bc
zhouyichen <zhouyichen123@gmail.com>
2015-11-01 16:06:08 +0800

    Modify adventure and quest views for UI

--- guildmasters/app/assets/javascripts/controllers/adventurer.js
+++ guildmasters/app/assets/javascripts/controllers/adventurer.js
@@ -1,29 +1,26 @@
-adventurers_list = {
-	adventurers : [
-		{
-			id : 1,
-			name : 'Ironman',
-			hp: 10,
-			max_hp: 15,
-			energy: 20,
-			max_energy: 30,
-			attack: 10,
-			defense: 20,
-			vision: 10,
-			state: 'free'
-		},
-		{
-			id : 2,
-			name : 'Hulk',
-			hp: 12,
-			max_hp: 20
-		}
-	]
-}
-
 Handlebars.registerPartial('adventurer',  HandlebarsTemplates['adventurer/adventurer']);
-var adventurersView = HandlebarsTemplates['adventurer/adventurer_list'];
-// $(function(){
-// 	var view = adventurersView(adventurers_list);
-// 	$('#mainContainer').html(view);
-// })
+var adventurersListTemplate =  HandlebarsTemplates['adventurer/adventurer_list'];
+GM.AdventurerController = {
+	adventurers_list : {
+		adventurers : [
+			{
+				id : 1,
+				name : 'Ironman',
+				hp: 10,
+				max_hp: 15,
+				energy: 20,
+				max_energy: 30,
+				attack: 10,
+				defense: 20,
+				vision: 10,
+				state: 'free'
+			},
+			{
+				id : 2,
+				name : 'Hulk',
+				hp: 12,
+				max_hp: 20
+			}
+		]
+	},
+};
--- guildmasters/app/assets/javascripts/controllers/quest.js
+++ guildmasters/app/assets/javascripts/controllers/quest.js
@@ -1,32 +1,29 @@
-quest_list = {
-	quests:[
-			{
-				id : 1,
-				description: 'You need to kill the monster at the cave!',
-				difficulty: 5,
-				state: 'unassigned',
-				reward: 10
-
-			},
-			{
-				id : 2,
-				difficulty: 8,
-				state: 'unassigned',
-				reward: 12
-
-			},
-			{
-				id : 3,
-				difficulty: 2,
-				state: 'unassigned',
-				reward: 4
-			}
-		]
-};
-
 Handlebars.registerPartial('quest',  HandlebarsTemplates['quest/quest']);
-var questsView = HandlebarsTemplates['quest/quest_list'];
-// $(function(){
-// 	var view = questsView(quest_list);
-// 	$('#mainContainer').html(view);
-// })
+var questsListTemplate = HandlebarsTemplates['quest/quest_list'];
+GM.QuestController = {
+	quest_list : {
+		quests:[
+				{
+					id : 1,
+					description: 'You need to kill the monster at the cave!',
+					difficulty: 5,
+					state: 'unassigned',
+					reward: 10
+
+				},
+				{
+					id : 2,
+					difficulty: 8,
+					state: 'unassigned',
+					reward: 12
+
+				},
+				{
+					id : 3,
+					difficulty: 2,
+					state: 'unassigned',
+					reward: 4
+				}
+			]
+	},
+};
