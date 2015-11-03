47d83745946eb872348c9f59f0594a75b4eba193
He Buwei <hebuweitom@gmail.com>
2015-11-03 13:59:00 +0800

    update seeds

--- guildmasters/db/seeds.rb
+++ guildmasters/db/seeds.rb
@@ -2,17 +2,30 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 
-5.times do |i|
-  Adventurer.create(hp: 100*i, 
-  	                max_hp: 100*i+200, 
-  	                energy: 50*i, 
-  	                max_energy: 60*i,
-  	                attack: 100,
-  	                defense: 50+i,
-  	                vision: i,
-  	                state: "available")
+gm = Guildmaster.create(gold: 20,
+	                    game_time: 10000)
 
-  Quest.create(difficulty: i,
+guild = gm.guilds.create(level: 3, popularity: 30)
+
+
+5.times do |i|
+  guild.adventurers.create(hp: 100+i, 
+  	                    max_hp: 120+i, 
+  	                    energy: 50+i, 
+  	                    max_energy: 60+i,
+  	                    attack: 100,
+  	                    defense: 50+i,
+  	                    vision: 100+i,
+  	                    state: "available",
+  	                    name: "adventurer#{i}")
+
+  Quest.create(difficulty: 1+i,
   	           state: "ongoing",
   	           reward: 100+i)
 end
+
+AdventurerTemplate.create(max_hp: 1000,
+	                      max_energy: 1000,
+	                      attack: 100,
+	                      defense: 100,
+	                      vision: 100)
