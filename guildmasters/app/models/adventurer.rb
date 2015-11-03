8e3fa812563bd325fbc3f7821ac687145fb43a5d
dtky <danieltangkengyan@gmail.com>
2015-11-03 14:12:05 +0800

    removed hardcode in model:adventurer.rb

--- guildmasters/app/models/adventurer.rb
+++ guildmasters/app/models/adventurer.rb
@@ -9,8 +9,8 @@ class Adventurer < ActiveRecord::Base
 	
 	def self.generate
 	  template = AdventurerTemplate.find(1)
-	  #level = Guild.find(1).level
-	  level = 1
+	  level = Guild.find(1).level
+	  #level = 1
 	  adventurer = Adventurer.new
 	  adventurer.name = "Alex"
 	  adventurer.max_hp = level*template.max_hp
@@ -21,8 +21,8 @@ class Adventurer < ActiveRecord::Base
 	  adventurer.defense = level*template.defense
 	  adventurer.vision = level*template.vision
 	  adventurer.state = "Pending"
-	  #adventurer.guild_id = Guild.find(1).id
-	  adventurer.guild_id = 1
+	  adventurer.guild_id = Guild.find(1).id
+	 # adventurer.guild_id = 1
 	  adventurer.save
 	  return adventurer
 	end
