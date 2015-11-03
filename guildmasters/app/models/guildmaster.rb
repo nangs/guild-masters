e1af4a1786d1047df8d8b5c398a6cc3526223276
Hu Yifei <ascalon0823@gmail.com>
2015-11-03 11:08:52 +0800

    Generate template in initialize function

--- guildmasters/app/models/guildmaster.rb
+++ guildmasters/app/models/guildmaster.rb
@@ -6,6 +6,16 @@ class Guildmaster < ActiveRecord::Base
 	  gm.gold=1000
 	  gm.game_time = 0
 	  gm.state="free"
+	  gm.save
+	  template = AdventurerTemplate.new
+	  template.max_hp = 100
+	  template.max_energy = 100
+	  template.attack = 1000
+	  template.defense=1000
+	  template.vision =1000
+	  template.region_id = 1
+	  template.save
+	  return gm
 	end
 	
 end
