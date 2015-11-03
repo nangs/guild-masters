75c5af7fd4cd4f6c427c3e11909a8f794332f3b3
Hu Yifei <ascalon0823@gmail.com>
2015-11-03 10:50:24 +0800

    Function generate implemented for adventurer and quest Function initialize implemented for guild and guildmaster Function view all implemented for adventurer and quest

--- guildmasters/app/models/guild.rb
+++ guildmasters/app/models/guild.rb
@@ -2,4 +2,13 @@ class Guild < ActiveRecord::Base
 	belongs_to :guildmaster
 	has_many :quests, dependent: :destroy
 	has_many :adventurers, dependent: :destroy
+	
+	def initialize
+	  guild=Guild.new
+	  guild.level = 1
+	  guild.popularity = 50
+	  guild.guildmaster_id = 1
+	  guild.save
+	  return guild
+	end
 end
--- guildmasters/app/models/guildmaster.rb
+++ guildmasters/app/models/guildmaster.rb
@@ -1,3 +1,11 @@
 class Guildmaster < ActiveRecord::Base
 	has_many :guilds, dependent: :destroy
+	
+	def initialize
+	  gm=Guildmaster.new
+	  gm.gold=1000
+	  gm.game_time = 0
+	  gm.state="free"
+	end
+	
 end
