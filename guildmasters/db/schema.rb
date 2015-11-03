47b481c3798207b9183613f502dcf1fd7ee8c813
He Buwei <hebuweitom@gmail.com>
2015-11-03 09:53:44 +0800

    add "name" attribute to facility

--- guildmasters/db/schema.rb
+++ guildmasters/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027052048) do
+ActiveRecord::Schema.define(version: 20151103014330) do
 
   create_table "adventurer_templates", force: :cascade do |t|
     t.integer  "max_hp"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20151027052048) do
     t.integer  "level"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "guildmasters", force: :cascade do |t|
@@ -88,6 +89,12 @@ ActiveRecord::Schema.define(version: 20151027052048) do
 
   create_table "regions", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "scouts", force: :cascade do |t|
+    t.string   "scout_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
