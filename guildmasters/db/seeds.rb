f59f152ff1cf0a3bf76d5345ebc5cc13911ede73
He Buwei <hebuweitom@gmail.com>
2015-11-03 12:07:22 +0800

    added seeds for database

--- guildmasters/db/seeds.rb
+++ guildmasters/db/seeds.rb
@@ -1,7 +1,18 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do |i|
+  Adventurer.create(hp: 100*i, 
+  	                max_hp: 100*i+200, 
+  	                energy: 50*i, 
+  	                max_energy: 60*i,
+  	                attack: 100,
+  	                defense: 50+i,
+  	                vision: i,
+  	                state: "available")
+
+  Quest.create(difficulty: i,
+  	           state: "ongoing",
+  	           reward: 100+i)
+end
