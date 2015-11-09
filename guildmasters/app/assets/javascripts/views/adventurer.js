97bae85d4ce8d95a0de3c81341197856c5acd8fa
zhouyichen <zhouyichen123@gmail.com>
2015-11-09 22:14:58 +0800

    implement UI for displaying new adventurer arrived

--- guildmasters/app/assets/javascripts/views/adventurer.js
+++ guildmasters/app/assets/javascripts/views/adventurer.js
@@ -1,4 +1,6 @@
 Handlebars.registerPartial('adventurer',  HandlebarsTemplates['adventurer/adventurer']);
+var adventurerTemplate = HandlebarsTemplates['adventurer/adventurer'];
 var adventurersListTemplate = HandlebarsTemplates['adventurer/adventurer_list'];
+var adventurerNewTemplate = HandlebarsTemplates['adventurer/adventurer_new'];
 var adventurersTableTemplate = HandlebarsTemplates['adventurer/adventurer_table'];
 var adventurerNewButton = HandlebarsTemplates['adventurer/adventurer_get']();
