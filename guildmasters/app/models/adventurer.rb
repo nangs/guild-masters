bbfdf470b305b9fe5b1de9df8939fb201cc91de9
dtky <danieltangkengyan@gmail.com>
2015-11-09 18:15:50 +0800

    added random generation from an array for adventurer names

--- guildmasters/app/models/adventurer.rb
+++ guildmasters/app/models/adventurer.rb
@@ -6,13 +6,14 @@ class Adventurer < ActiveRecord::Base
 	  adventurers = Adventurer.all
 	  return adventurers
 	end
-	
+
 	def self.generate
+		@adventurer_names = ["Alex", "Daniel", "David", "John", "Ben", "Charles", "William", "Alvin", "Petter", "Nathan", "Howard" ]
 	  template = AdventurerTemplate.find(1)
 	  level = Guild.find(1).level
 	  #level = 1
 	  adventurer = Adventurer.new
-	  adventurer.name = "Alex"
+	  adventurer.name = @adventurer_names.sample
 	  adventurer.max_hp = level*template.max_hp
 	  adventurer.hp=adventurer.max_hp
 	  adventurer.max_energy = level*template.max_energy
