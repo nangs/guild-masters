dd1ecea447c576affa9d5403526b74d47043a607
zhouyichen <zhouyichen123@gmail.com>
2015-11-16 18:45:01 +0800

    Update the schema

--- guildmasters/db/schema.rb
+++ guildmasters/db/schema.rb
@@ -100,11 +100,16 @@ ActiveRecord::Schema.define(version: 20151116061417) do
   end
 
   add_index "quests", ["guild_id"], name: "index_quests_on_guild_id"
-  #Extra line after migration, unknown purpose
   add_index "quests", ["quest_event_id"], name: "index_quests_on_quest_event_id"
 
   create_table "regions", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "scouts", force: :cascade do |t|
+    t.string   "scout_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
