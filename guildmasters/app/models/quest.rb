e334f40800315a5d7500ea9c92b7da061d0d4c9b
Hu Yifei <ascalon0823@gmail.com>
2015-11-16 13:03:20 +0800

    Randomize quest and adventurer generation Assign function fix

--- guildmasters/app/models/quest.rb
+++ guildmasters/app/models/quest.rb
@@ -1,43 +1,66 @@
 class Quest < ActiveRecord::Base
-	belongs_to :guild
-	has_many :adventurers
+  belongs_to :guild
+  has_many :adventurers
+  #This function returns a list of Quests to the controller
+  def self.view_all
+    quests=Quest.all
+    return quests
+  end
 
-	#This function returns a list of Quests to the controller
-	def self.view_all
-	  quests=Quest.all
-	  return quests
-	end
+  #This function creates and saves a Quest into the Database
+  #It will return the newly created Quest to the controller
+  def self.generate
+    #level = Guild.find(1).level
+    level = 1
+    quest = Quest.new
+    quest.difficulty = level+Random.rand(2)
+    quest.state = "pending"
+    quest.reward = 1000*quest.difficulty+Random.rand(1000)
+    #quest.guild_id=Guild.find(1).id
+    quest.guild_id = 1
+    quest.save
+    return quest
+  end
 
-	#This function creates and saves a Quest into the Database
-	#It will return the newly created Quest to the controller
-	def self.generate
-	  #level = Guild.find(1).level
-	  level = 1
-	  quest = Quest.new
-	  quest.difficulty = level
-	  quest.state = "pending"
-	  quest.reward = 1000*quest.difficulty
-	  #quest.guild_id=Guild.find(1).id
-	  quest.guild_id = 1
-	  quest.save
-	  return quest
-	end
-	
-	def self.get(_quest_id)
-	  quest = Quest.find(_quest_id)
-	  return quest
-	end
-	
-	def self.assign(_quest_id,_adventurer_ids)
-	  adventurers = Adventurer.find(_adventurer_ids)
-	  quest=Quest.find(_quest_id)
-	  quest.adventurers = adventurers
-	  adventurers.state = "assigned"
-	  adventurers.quest = quest
-	  quest.state = "assigned"
-	  quest.save
-	  adventurers.save
-	  return quest
-	end
-	
+  def self.get(_quest_id)
+    quest = Quest.find(_quest_id)
+    return quest
+  end
+
+  def self.assign(_quest_id,_adventurer_ids)
+    for id in _adventurers_ids
+      adventurer = Adventurer.find(id)
+      quest=Quest.find(_quest_id)
+      adventurer.state = "assigned"
+      adventurer.quest = quest
+      adventurer.save
+      quest.adventurers<<adventurer
+    end
+    quest.state = "assigned"
+    quest.save
+    return quest
+  end
+
+  def self.complete(_quest_id)
+    quest = Quest.find(_quest_id)
+    adventurers = quest.adventurers
+    sum=0
+    for adventurer in adventurers
+      sum = sum + adventurer.attack + adventurer.defense + adventurer.vision
+      adventurer.energy = adventurer.enegegy - quest.difficulty*100-Random.rand(50)
+      adventurer.state = "available"
+      adventurer.save
+    end
+    sum = sum/900
+    gm = Guildmaster.find(1)
+    if(sum>=quest.difficulty)
+      quest.state="successful"
+    gm.gold = gm.gold+quest.reward
+    else
+      quest.state="failed"
+    end
+    gm.game_time = gm.game_time+quest.difficulty*100
+    gm.save
+    quest.save
+  end
 end
--- guildmasters/test/models/quest_test.rb
+++ guildmasters/test/models/quest_test.rb
@@ -4,4 +4,18 @@ class QuestTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "assign" do
+    adventurer = adventurers(:one)
+    quest=quests(:one)
+    adventurer.state = "assigned"
+    adventurer.quest = quest
+    quest.adventurers<<adventurer
+    adventurer = adventurers(:two)
+    adventurer.state = "assigned"
+    adventurer.quest = quest
+    quest.adventurers<<adventurer
+    quest.state = "assigned"
+     
+    puts quest.adventurers.inspect
+  end
 end
