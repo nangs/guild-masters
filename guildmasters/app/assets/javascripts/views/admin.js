52cd95bc2c64eb3ac6875fffa2c6ed6579643b86
zhouyichen <zhouyichen123@gmail.com>
2016-02-03 17:28:56 +0800

    Create UI template for reseting password

--- guildmasters/app/assets/javascripts/views/admin.js
+++ guildmasters/app/assets/javascripts/views/admin.js
@@ -2,3 +2,4 @@ var signupTemplate = HandlebarsTemplates['admin/signup'];
 var signupSuccessTemplate = HandlebarsTemplates['admin/signupSuccess'];
 var activateSuccessTemplate = HandlebarsTemplates['admin/activateSuccess'];
 var loginTemplate = HandlebarsTemplates['admin/login'];
+var resetPasswordTemplate = HandlebarsTemplates['admin/resetPassword'];
