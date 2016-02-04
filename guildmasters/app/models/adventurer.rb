6ad6e9bebe975db5bc80b5171018e93fa82a591f
Hu Yifei <ascalon0823@gmail.com>
2016-02-04 14:19:17 +0800

    Remove generate function from adventurer, add interface. Energy cost calculation integrated with battle calculation in quest.

--- guildmasters/app/models/adventurer.rb
+++ guildmasters/app/models/adventurer.rb
@@ -11,34 +11,8 @@ class Adventurer < ActiveRecord::Base
 	def self.random_adventurer_name
 		return File.readlines(Rails.root.join "app", "models", "adventurerNames.txt").sample
 	end
-
-  #This function creates and saves an Adventurer into the Database
-  #It will return the newly created Adventurer to the controller
-  def self.generate
-    template = AdventurerTemplate.find(1)
-    level = 3
-    adventurer = Adventurer.new
-    adventurer.name = random_adventurer_name
-    adventurer.max_hp = level*template.max_hp+Random.rand(1000)
-    adventurer.hp=adventurer.max_hp
-    adventurer.max_energy = level*template.max_energy+Random.rand(1000)
-    adventurer.energy = adventurer.max_energy
-    adventurer.attack = level*template.attack+Random.rand(100)
-    adventurer.defense = level*template.defense+Random.rand(100)
-    adventurer.vision = level*template.vision+Random.rand(100)
-    adventurer.state = "Available"
-    adventurer.guild_id = Guild.find(1).id
-    adventurer.save
-    return adventurer
-  end
-  
-  #This function will compute the energy cost after completing a quest and make them available
-  def return
-    self.energy = self.energy - self.quest.difficulty*100-Random.rand(self.quest.difficulty*50)
-    if(self.energy<=0)
-      self.energy=0
-    end
-    self.state = "Available"
-    self.save
+  def self.generate(guild_id)
+    guild = Guild.find(guild_id)
+    return guild.create_adventurer
   end
 end
