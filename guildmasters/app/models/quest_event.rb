d32be9cd730fe33cf33c04d0a6135ab2c6486811
Hu Yifei <ascalon0823@gmail.com>
2016-02-04 14:31:13 +0800

    Remove setup function for quest event Refactor complete function

--- guildmasters/app/models/quest_event.rb
+++ guildmasters/app/models/quest_event.rb
@@ -7,20 +7,10 @@ class QuestEvent < ActiveRecord::Base
     return events
   end
   
-  #This function will construct a QuestEvent
-  def setup(quest)
-    self.quest_id = quest.id
-    self.start_time = quest.guild.guildmaster.game_time
-    self.end_time = self.start_time + quest.difficulty*100+Random.rand(quest.difficulty*25)
-    self.gold_spent = 0
-    self.save
-  end
-  
   #This is a interface that calls completion of quest of the event
   #Return result of the Quest
-  def self.complete(event_id)
-    event = QuestEvent.find(event_id)
-    quest = event.quest
+  def complete
+    quest = self.quest
     return quest.complete   
   end
 end
