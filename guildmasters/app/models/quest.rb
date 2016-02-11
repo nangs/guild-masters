324df128de1b9b01ad0472be0ef875075f946f53
Hu Yifei <ascalon0823@gmail.com>
2016-02-11 12:01:31 +0800

    Move part of assign function to quest event Move complete function to quest event Fix adventurers lost bug in battle function Move time cost function to quest event

--- guildmasters/app/models/quest.rb
+++ guildmasters/app/models/quest.rb
@@ -1,21 +1,21 @@
 require 'monster.rb'
+
 class Quest < ActiveRecord::Base
   belongs_to :guild
   has_many :quest_events
   belongs_to :monster_template
-  
   #This function returns a list of Quests to the controller
   def self.view_all
     quests=Quest.all
     return quests
   end
-  
+
   #This function is a interface to create a new quest
   def self.generate(guild_id)
     guild = Guild.find(guild_id)
     return guild.create_quest
   end
-  
+
   #This function is a interface to assign adventurers to quest, it calls the actuall logic
   #Return the result of assigning quest
   def self.assign(quest_id,adventurer_ids)
@@ -27,125 +27,81 @@ class Quest < ActiveRecord::Base
   #This function will check the availability of Quest and Adventures and assign them togather and generate a Quest event
   def assign(adventurers)
     error_message = "Error, not available"
-    
+
     #Check Quest Status. Done by front end too
     if(self.state == "assigned"||self.state=="successful")
-      return error_message
+    return error_message
     end
-    self.adventurers.clear
-    
+
     #Check Adventurers Status. Done by front end too
     adventurers.each do |adventurer|
       if(adventurer.state =="assigned"||adventurer.state=="dead"||adventurer.energy<=0)
-        return error_message
-      else
-        adventurer.state = "assigned"
-        adventurer.quest = self
-        adventurer.save
+      return error_message
       end
     end
     self.state = "assigned"
-    self.adventurers = adventurers
-    
+
     #Generate quest event
-    self.create_quest_event(start_time: self.guild.guildmaster.game_time,
-                            end_time: self.guild.guildmaster.game_time + self.time_cost, 
-                            gold_spent: 0)
+    self.quest_events << QuestEvent.assign(self,adventurers)
     self.save
     return "Successfully assigned"
   end
 
-  #This function compute the result of quest, clearing the association and return message of result
-  def complete
-    
-    #Relief adventurers and calculate quest factor
-    adventurers = self.adventurers
-    result = self.battle
-
-    guild = self.guild
-    gm=guild.guildmaster
-    
-    #Success/Fail judgement
-    if(result)
-      self.state="successful"
-      guild.popularity=guild.popularity+self.difficulty
-      gm.gold = gm.gold+self.reward
-      msg= "Quest completed! Your guild earned %d gold and %d popularity!" % [self.reward,self.difficulty]
-    else
-      self.state="failed"
-      guild.popularity=guild.popularity-self.difficulty
-      msg = "Quest failed! Your guild lost %d popularity" % self.difficulty
-    end
-    
-    guild.save
-    gm.game_time = self.quest_event.end_time
-    gm.save
-    self.adventurers.clear
-    self.save
-    self.quest_event=nil
-    return msg
-  end
-  
-  #This function calculates the game time needed for the quest to complete
-  def time_cost
-    adv_vision=0
-    adventurers = self.adventurers
-    for adventurer in adventurers
-      adv_vision = adv_vision + adventurer.vision
-    end
-    mon_invis = self.difficulty*self.monster_template.invisibility
-    return 100*self.difficulty+100*mon_invis/adv_vision
-  end
-  
   #This function simulates the battle process when carry out a quest
-  def battle
-    
-    adventurers = self.adventurers
+  def battle(adventurers)
     #Generate monster instance according to template
+    advs = adventurers.clone
+    survivers = (0..advs.size-1).to_a
     monster = Monster.new(self.monster_template,self.difficulty)
+    puts advs.inspect
+    puts monster.inspect
     end_of_battle=false
     adv_victory = false
     turn = 0
     while(!end_of_battle)
       turn = turn+1
+      puts turn
       #Adventurers` phase
-      for adventurer in adventurers
-        if(adventurer.hp>0)
-          if(monster.hp>0)
-            monster.hp=monster.hp-(adventurer.attack*adventurer.attack/monster.defense)
-            if(monster.hp<0)
-              monster.hp=0
-            end
+      for adv_id in survivers
+        adventurer = adventurers[adv_id]
+        if(monster.hp>0)
+          monster.hp=monster.hp-(adventurer.attack*adventurer.attack/monster.defense)
+          puts "adventurer: %d >> monster: %d " % [adventurer.hp, monster.hp]
+          if(monster.hp<0)
+            monster.hp=0
           end
         end
       end
       #Monster`s phase
       if(monster.hp>0)
-        target = adventurers.sample
+        target_id = survivers.sample
+        target=advs[target_id]
         target.hp = target.hp - (monster.attack*monster.attack/target.defense)
+        puts "monster: %d >> adventurers %d" % [monster.hp,target.hp]
         #Adventurer was killed
         if(target.hp<=0)
           target.hp=0
           target.energy=0
           target.state = "dead"
-          target.save
-          adventurers.delete(target)
-          if(adventurers.empty?)
-            end_of_battle=true
+          survivers.delete(target_id)
+          if(survivers.empty?)
+          end_of_battle = true
           end
         end
       elsif(monster.hp==0)
         end_of_battle = true
         adv_victory=true
       end
+
     end
     #Energy cost calculation
-    if(!adventurers.empty?)
-      for adventurer in adventurers
-        adventurer.energy = adventurer.energy - 10 - self.difficulty*5
-        adventurer.state = "available"
-      end
+    for adventurer in advs
+      adventurer.energy = adventurer.energy - 5 - self.difficulty*5
+      adventurer.state = "available" if adventurer.state == "assigned"
+      adventurer.energy = 0 if adventurer.energy<0
+      adventurer.save
     end
+    puts adv_victory
     return adv_victory
   end
 end
--- guildmasters/app/models/quest_event.rb
+++ guildmasters/app/models/quest_event.rb
@@ -7,11 +7,58 @@ class QuestEvent < ActiveRecord::Base
     events = QuestEvent.all
     return events
   end
-  
-  #This is a interface that calls completion of quest of the event
-  #Return result of the Quest
+  #This function compute the result of quest, clearing the association and return message of result
   def complete
-    quest = self.quest
-    return quest.complete   
+    
+    #Relief adventurers and calculate quest factor
+    result = self.quest.battle(self.adventurers)
+
+    guild = self.quest.guild
+    gm=guild.guildmaster
+    
+    #Success/Fail judgement
+    if(result)
+      self.quest.state="successful"
+      guild.popularity=guild.popularity+self.quest.difficulty
+      gm.gold = gm.gold+self.quest.reward
+      msg= "Quest completed! Your guild earned %d gold and %d popularity!" % [self.quest.reward,self.quest.difficulty]
+    else
+      self.quest.state="failed"
+      guild.popularity=guild.popularity-self.quest.difficulty
+      msg = "Quest failed! Your guild lost %d popularity" % self.quest.difficulty
+    end
+    
+    guild.save
+    gm.game_time = self.end_time
+    gm.save
+    self.save
+    return msg
+  end
+  
+  #This function will create the relationship between adventurer and quest_event and quest
+  def self.assign(quest,adventurers)
+    qe= QuestEvent.new
+    adventurers.each do |adventurer|
+      adventurer.state = "assigned"
+      adventurer.quest_events << qe
+      adventurer.save
+    end
+    qe.quest=quest
+    qe.start_time = quest.guild.guildmaster.game_time
+    qe.end_time = qe.start_time + qe.time_cost
+    qe.gold_spent=0
+    qe.save
+    return qe
+  end
+  
+  #This function calculates the game time needed for the quest to complete
+  def time_cost
+    adv_vision=0
+    adventurers = self.adventurers
+    for adventurer in adventurers
+      adv_vision = adv_vision + adventurer.vision
+    end
+    mon_invis = self.quest.difficulty*self.quest.monster_template.invisibility
+    return 100*self.quest.difficulty+100*mon_invis/adv_vision
   end
 end
