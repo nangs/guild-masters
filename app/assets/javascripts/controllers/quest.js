3cda010024736efc32f55c632f96cb5d70376aaf
zhouyichen <zhouyichen123@gmail.com>
2016-02-20 22:38:26 +0800

    Fix displaying of quest when assigning adventurers

--- app/assets/javascripts/controllers/quest.js
+++ app/assets/javascripts/controllers/quest.js
@@ -3,10 +3,18 @@ GM.QuestController = Ember.Controller.extend();
 GM.QuestController.showAssign = function(id) {
 	// Temporary way of getting the quest. 
 	// The API should be designed in a way that a particular Quest can be directly retrieved by id.
-	quest = GM.QuestModel.quest_list['quests'][id-1];
+	var quests = GM.QuestModel.quest_list['quests'];
+	var quest;
+	for (var q in quests) {
+		if (quests[q]['id'] == id) {
+			quest = quests[q];
+		}
+	}
 	var questView = questAssignTemplate(quest);
-	var adventurersView = adventurerAssignTemplate(GM.AdventurerModel.adventurers_list);
-	showView(questView + adventurersView);
+	GM.AdventurerModel.getAllAdventurers(function () {
+		var adventurersView = adventurerAssignTemplate(GM.AdventurerModel.adventurers_list);
+		showView(questView + adventurersView);	
+	});
 }
 
 GM.QuestController.assign = function(id) {
