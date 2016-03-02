66b9fd6592cd06187c9ea48963ed9cb62bd272eb
zhouyichen <zhouyichen123@gmail.com>
2016-03-02 18:13:04 +0800

    Add starting circle for time bar, and reduce height

--- app/assets/javascripts/views/timeBar.js
+++ app/assets/javascripts/views/timeBar.js
@@ -1,11 +1,21 @@
 function setupTimeBar(events, currentGameTime){
 	console.log(events, currentGameTime);
 	var canvas = new fabric.StaticCanvas('timeBar');
-	
-	var timeBar = new fabric.Line([0, 20, 1500, 20], {
-		left: 70,
+	var start_point = 20;
+
+	var timeBar = new fabric.Line([start_point, 20, 1500, 20], {
 		strokeWidth: 10,
 		stroke: 'green'
 	});
-	canvas.add(timeBar);
+
+	var start = new fabric.Circle({
+		top : 20,
+		left : start_point,
+		strokeWidth: 10,
+		radius: 10,
+		fill: '#fff',
+		stroke: 'green'
+	});
+
+	canvas.add(timeBar, start);
 }
