2ae1b705961a3588f2f2e62245a2f6d78db8bc09
zhouyichen <zhouyichen123@gmail.com>
2016-03-03 10:27:05 +0800

    Add quest event circle and end of day line of the time bar

--- app/assets/javascripts/views/timeBar.js
+++ app/assets/javascripts/views/timeBar.js
@@ -1,21 +1,57 @@
-function setupTimeBar(events, currentGameTime){
+var timeBar_start_point = 20;
+var timeBar_top_padding = 20;
+function renderTimeBar(events, currentGameTime){
 	console.log(events, currentGameTime);
 	var canvas = new fabric.StaticCanvas('timeBar');
-	var start_point = 20;
-
-	var timeBar = new fabric.Line([start_point, 20, 1500, 20], {
+	
+	var timeBar = new fabric.Line([timeBar_start_point, timeBar_top_padding, 1100, timeBar_top_padding], {
 		strokeWidth: 10,
 		stroke: 'green'
 	});
 
 	var start = new fabric.Circle({
-		top : 20,
-		left : start_point,
+		top : timeBar_top_padding,
+		left : timeBar_start_point,
 		strokeWidth: 10,
 		radius: 10,
 		fill: '#fff',
 		stroke: 'green'
 	});
 
-	canvas.add(timeBar, start);
+	var nextDay = Math.floor(currentGameTime / 1000 + 1) * 1000;
+	var nextDayPoint = nextDay - currentGameTime + timeBar_start_point;
+	console.log(nextDayPoint);
+	var endOfDay = new fabric.Line([nextDayPoint , timeBar_top_padding - 10, nextDayPoint, timeBar_top_padding + 10], {
+		strokeWidth: 10,
+		stroke: 'red'
+	});
+
+	canvas.add(timeBar, start, endOfDay);
+	for (var e in events) {
+		var eve = events[e];
+		var newEndCircle;
+		switch (eve.type) {
+			case "QuestEvent":
+				newEndCircle = renderQuestCircle(eve, currentGameTime);
+				break;
+			case "FacilityEvent":
+				newEndCircle = renderFacilityCircle(eve, currentGameTime);
+				break;
+		}
+		canvas.add(newEndCircle);
+	}
+	return canvas;
+}
+
+function renderQuestCircle(eve, currentGameTime) {
+	var endTime = eve.end_time - currentGameTime;
+	var circle = new fabric.Circle({
+		top : timeBar_top_padding,
+		left : endTime + timeBar_start_point,
+		strokeWidth: 10,
+		radius: 10,
+		fill: '#fff',
+		stroke: 'blue'
+	});
+	return circle;
 }
