3f9c192887be2accdda330675afaaad7d59fcf86
dtky <danieltangkengyan@gmail.com>
2016-03-03 10:33:16 +0800

    fix typo in finding facility_id

--- app/controllers/facilityevents_controller.rb
+++ app/controllers/facilityevents_controller.rb
@@ -20,7 +20,7 @@ class FacilityeventsController < ApplicationController
     acc = Account.find(session[:account_id])
     guildmaster = acc.guildmaster
     guild = Guild.find(guildmaster.current_guild_id)
-    @facility = guild.facilities.find_by(name: params[:facility_id])
+    @facility = guild.facilities.find_by(id: params[:facility_id])
     adventurers = Adventurer.find(params[:adventurers_ids])
     assign_facility = FacilityEvent.assign(@facility,adventurers)
     respond_to do |format|
