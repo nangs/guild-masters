9d2073373506e9674b388139c2a4382ce02ab369
Hu Yifei <ascalon0823@gmail.com>
2016-03-03 11:49:03 +0800

    Implement complete function for next event and all events until certain time

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,24 @@
+require 'facility_event.rb'
+require 'quest_event.rb'
 class Event < ActiveRecord::Base
+  def self.complete_next(gm)
+    end_time=gm.quest_events.where("end_time > ?", gm.game_time).first.end_time
+    temp = gm.facility_events.where("end_time > ?",gm.game_time).first.end_time
+    end_time = temp if temp < end_time
+    return Event.complete(gm,end_time)
+  end
   
-
-  def assign_quest(quest,start_time)
-    @start_time = start_time
-    @end_time = start_time + quest.difficulty*100
-    @gold_spent = 0
-    @type = "quest"  
+  def self.complete(gm,end_time)
+    qes=gm.quest_events.where(end_time: gm.game_time..end_time)
+    fes=gm.facility_events.where(end_time: gm.game_time..end_time)
+    for qe in qes
+      qe.complete
+    end
+    for fe in fes
+      fe.complete
+    end
+    gm.game_time=end_time
+    gm.save
+    return {msg: "successful"}
   end
 end
