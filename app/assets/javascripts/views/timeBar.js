773af76772de675efe4fe2e199bb042a2e304131
zhouyichen <zhouyichen123@gmail.com>
2016-03-04 12:58:43 +0800

    Prepare to show info for events on time bar

--- app/assets/javascripts/views/timeBar.js
+++ app/assets/javascripts/views/timeBar.js
@@ -1,9 +1,8 @@
 var timeBar_start_point = 20;
 var timeBar_top_padding = 20;
 function renderTimeBar(events, currentGameTime){
-	console.log(events, currentGameTime);
-	var canvas = new fabric.StaticCanvas('timeBar');
-	
+	var canvas = new fabric.Canvas('timeBar');
+
 	var timeBar = new fabric.Line([timeBar_start_point, timeBar_top_padding, 1100, timeBar_top_padding], {
 		strokeWidth: 10,
 		stroke: 'green'
@@ -20,38 +19,79 @@ function renderTimeBar(events, currentGameTime){
 
 	var nextDay = Math.floor(currentGameTime / 1000 + 1) * 1000;
 	var nextDayPoint = nextDay - currentGameTime + timeBar_start_point;
-	console.log(nextDayPoint);
 	var endOfDay = new fabric.Line([nextDayPoint , timeBar_top_padding - 10, nextDayPoint, timeBar_top_padding + 10], {
 		strokeWidth: 10,
 		stroke: 'red'
 	});
 
 	canvas.add(timeBar, start, endOfDay);
-	for (var e in events) {
-		var eve = events[e];
-		var newEndCircle;
-		switch (eve.type) {
-			case "QuestEvent":
-				newEndCircle = renderQuestCircle(eve, currentGameTime);
-				break;
-			case "FacilityEvent":
-				newEndCircle = renderFacilityCircle(eve, currentGameTime);
-				break;
-		}
-		canvas.add(newEndCircle);
+	if (events.length > 0) {
+		for (var e in events) {
+			var eve = events[e];
+			var newEndMark;
+			switch (eve.type) {
+				case "QuestEvent":
+					renderQuestMark(eve, currentGameTime, canvas);
+					break;
+				case "FacilityEvent":
+					renderFacilityMark(eve, currentGameTime, canvas);
+					break;
+			}
+		}	
 	}
+	canvas.on('mouse:move', function(e) {
+		if (e.target) {
+			if (!e.target.isInfoShown) {
+				if (e.target.showInfo) {
+					e.target.showInfo();
+				}
+			}
+		} else {
+			canvas.forEachObject(function(o) {
+				if (o.removeInfo) {
+					o.removeInfo();
+					o.isInfoShown = false;
+				}
+			});
+		}
+		canvas.renderAll();
+	});
+	canvas.forEachObject(function(o) {
+		o.selectable = false;
+	});
 	return canvas;
 }
 
-function renderQuestCircle(eve, currentGameTime) {
+function renderQuestMark(eve, currentGameTime, canvas) {
 	var endTime = eve.end_time - currentGameTime;
 	var circle = new fabric.Circle({
 		top : timeBar_top_padding,
 		left : endTime + timeBar_start_point,
-		strokeWidth: 10,
-		radius: 10,
+		strokeWidth: 8,
+		radius: 8,
 		fill: '#fff',
 		stroke: 'blue'
 	});
-	return circle;
+	circle.isInfoShown = false;
+
+	circle.showInfo = function () {
+		var text = new fabric.Text("Info box", {
+			left: endTime + timeBar_start_point + 20,
+			top: timeBar_top_padding + 16,
+			fontSize: 10
+		});
+		text.set('backgroundColor', 'rgb(200,200,0)');
+		text.setOpacity(0.5);
+		canvas.add(text);
+		text.selectable = false;
+		circle.isInfoShown = true;
+		circle.infoText = text;
+	};
+
+	circle.removeInfo = function () {
+		canvas.remove(this.infoText);
+	};
+
+	circle.selectable = false;
+	canvas.add(circle);
 }
