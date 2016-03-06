18ea1168c2f3426da08cff46fe325fa9160876ae
dtky <danieltangkengyan@gmail.com>
2016-03-07 01:00:35 +0800

    implement accounts controller test for index and create:"signup"

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe Account do
-  it "has a valid factory" do
+  it 'has a valid factor' do
     expect(FactoryGirl.create(:account)).to be_valid
   end
-  it "is invalid without password" do
+  it 'is invalid without password' do
     expect{ FactoryGirl.create(:account, password: nil) }.to raise_error(ActiveRecord::RecordInvalid)
   end
 end
