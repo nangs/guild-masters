07d364152f0878012d5469197dd680b49385b6d3
dtky <danieltangkengyan@gmail.com>
2016-03-10 09:29:25 +0800

    add more test cases for account_spec

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
+#rspec spec/models/account_spec.rb --format documentation
 
 describe Account do
   it 'has a valid factor' do
+    expect(Account.count).to eq(0)
     expect(FactoryGirl.create(:account)).to be_valid
+    expect(Account.count).to eq(1)
+  end
+  it 'has a valid factor without email' do
+    expect(Account.count).to eq(0)
+    expect(FactoryGirl.create(:account, email: nil)).to be_valid
+    expect(Account.count).to eq(1)
   end
   it 'is invalid without password' do
+    expect(Account.count).to eq(0)
     expect{ FactoryGirl.create(:account, password: nil) }.to raise_error(ActiveRecord::RecordInvalid)
+    expect(Account.count).to eq(0)
   end
 end
