1a406e6ef4fe14f0f1efc8117aaf9abc591b8343
dtky <danieltangkengyan@gmail.com>
2016-03-14 01:26:48 +0800

    move admin::adventurernamecontroller spec file to admin folder

--- spec/controllers/admin/adventurer_names_controller_spec.rb
+++ spec/controllers/admin/adventurer_names_controller_spec.rb
@@ -18,7 +18,7 @@ require 'rails_helper'
 # Message expectations are only used when there is no simpler way to specify
 # that an instance is receiving a specific message.
 
-RSpec.describe AdventurerNamesController, type: :controller do
+RSpec.describe Admin::AdventurerNamesController, type: :controller do
 
   # This should return the minimal set of attributes required to create a valid
   # AdventurerName. As you add validations to AdventurerName, be sure to
