cc8eed988afe816dc0f9edb3f4161b583b8209dd
zhouyichen <zhouyichen123@gmail.com>
2016-03-16 18:05:05 +0800

    Display details for successful quest

--- app/assets/javascripts/controllers/event.js
+++ app/assets/javascripts/controllers/event.js
@@ -1,1 +1,14 @@
 GM.EventController = Ember.Controller.extend();
+
+GM.EventController.showQuestResults = function (questEvents) {
+	var message = '';
+	for (var qe = 0; qe < questEvents.length; qe++) {
+		var questEvent = questEvents[qe];
+		if (questEvent.msg == "success") {
+			message += questSuccessTemplate(questEvent);
+		} else {
+			message += questFailTemplate(questEvent);
+		}
+	}
+	showView(message);
+}
--- app/assets/javascripts/views/quest.js
+++ app/assets/javascripts/views/quest.js
@@ -3,11 +3,13 @@ Handlebars.registerHelper('isCompleted', function(state, options){
 		return options.fn(this);
 	}
 	return options.inverse(this);
-});	
+});
 Handlebars.registerPartial('quest',  HandlebarsTemplates['quest/quest']);
 var questTemplate = HandlebarsTemplates['quest/quest'];
 var questsListTemplate = HandlebarsTemplates['quest/quest_list'];
 var questNewTemplate = HandlebarsTemplates['quest/quest_new'];
 var questsTableTemplate = HandlebarsTemplates['quest/quest_table'];
 var questNewButton = HandlebarsTemplates['quest/quest_get']();
-var questAssignTemplate = HandlebarsTemplates['quest/quest_assign'];var questFailTemplate = HandlebarsTemplates['quest/quest_result_fail'];
+var questAssignTemplate = HandlebarsTemplates['quest/quest_assign'];
+var questFailTemplate = HandlebarsTemplates['quest/quest_result_fail'];
+var questSuccessTemplate = HandlebarsTemplates['quest/quest_result_success'];
