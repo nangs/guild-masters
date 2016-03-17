81192439e8eb311e2338764a89850de94c77aed1
zhouyichen <zhouyichen123@gmail.com>
2016-03-17 11:51:46 +0800

    Show the scout event on the timebar

--- app/assets/javascripts/views/timeBar.js
+++ app/assets/javascripts/views/timeBar.js
@@ -51,6 +51,8 @@ function renderTimeBar(events, currentGameTime){
 				case "FacilityEvent":
 					renderFacilityMark(eve, currentGameTime, canvas);
 					break;
+				case "ScoutEvent":
+					renderScoutMark(eve, currentGameTime, canvas);
 			}
 		}	
 	}
@@ -142,6 +144,43 @@ function renderFacilityMark(eve, currentGameTime, canvas) {
 		// text.selectable = false;
 		// circle.isInfoShown = true;
 		// circle.infoText = text;
+	};
+
+	circle.removeInfo = function () {
+		canvas.remove(this.infoText);
+	};
+
+	circle.selectable = false;
+	canvas.add(circle);
+}
+
+function renderScoutMark(eve, currentGameTime, canvas) {
+	var endTime = eve.end_time - currentGameTime;
+	var circle = new fabric.Circle({
+		top : timeBar_top_padding,
+		left : endTime + timeBar_start_point,
+		strokeWidth: 8,
+		radius: 8,
+		fill: '#fff',
+		stroke: 'yellow'
+	});
+	circle.isInfoShown = false;
+
+	circle.showInfo = function () {
+		var quest = eve.quest;
+		var infoText = "Scouting";
+		var text = new fabric.Text(infoText, {
+			left: endTime + timeBar_start_point + 20,
+			top: timeBar_top_padding + 15,
+			fontSize: 12,
+			stroke: 'rgb(200,100,50)'
+		});
+		text.set('backgroundColor', 'rgb(0,200,200)');
+		text.setOpacity(0.8);
+		canvas.add(text);
+		text.selectable = false;
+		circle.isInfoShown = true;
+		circle.infoText = text;
 	};
 
 	circle.removeInfo = function () {
