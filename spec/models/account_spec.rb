a06c5caf0fa016f5578ccece1cd3284388f896e8
dtky <danieltangkengyan@gmail.com>
2016-03-17 22:38:25 +0800

    replace factorygirl.create to create because factorygirl syntax methods enable

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -4,17 +4,17 @@ require 'rails_helper'
 describe Account do
   it 'has a valid factor' do
     expect(Account.count).to eq(0)
-    expect(FactoryGirl.create(:account)).to be_valid
+    expect(create(:account)).to be_valid
     expect(Account.count).to eq(1)
   end
   it 'has a valid factor without email' do
     expect(Account.count).to eq(0)
-    expect(FactoryGirl.create(:account, email: nil)).to be_valid
+    expect(create(:account, email: nil)).to be_valid
     expect(Account.count).to eq(1)
   end
   it 'is invalid without password' do
     expect(Account.count).to eq(0)
-    expect{ FactoryGirl.create(:account, password: nil) }.to raise_error(ActiveRecord::RecordInvalid)
+    expect{create(:account, password: nil) }.to raise_error(ActiveRecord::RecordInvalid)
     expect(Account.count).to eq(0)
   end
 end
