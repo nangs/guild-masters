59e32918e8666b09fd2caff29023e6a51f03ef35
zhouyichen <zhouyichen123@gmail.com>
2016-03-27 21:30:54 +0800

    Refactor: move ajax call from controller to model in the frontend

--- app/assets/javascripts/controllers/guild.js
+++ app/assets/javascripts/controllers/guild.js
@@ -1,12 +1,1 @@
 GM.GuildController = Ember.Controller.extend();
-GM.GuildController.postGuildID = function (guildID) {
-    $.ajax({
-	    type: 'POST',
-	    url: 'guild_sessions.json',
-	    data: {
-	    	guild_id : guildID
-	    },
-	    success: function(feedback) {
-	    }
-	});
-}
