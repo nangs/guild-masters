e398ca671d0b0870b89f5a034e8c4f9be06e5111
dtky <danieltangkengyan@gmail.com>
2016-03-28 15:09:07 +0800

    implement test case for guild_controller

--- app/controllers/guild_controller.rb
+++ app/controllers/guild_controller.rb
@@ -2,25 +2,30 @@ class GuildController < ApplicationController
   respond_to :json
   before_action :authorize
 
-  ########
-  ########for testing not for release
-  ########
-  # GET /guild.json
-  def index
-    acc = Account.find(session[:account_id])
-    guildmaster = acc.guildmaster
-    guild = Guild.find(guildmaster.current_guild_id)
-    respond_to do |format|
-      format.json { render json: guild }
-    end
-  end
-  ########
-  ########for testing not for release
-  ########
+  # ########
+  # ########for testing not for release
+  # ########
+  # # GET /guild.json
+  # def index
+  #   acc = Account.find(session[:account_id])
+  #   guildmaster = acc.guildmaster
+  #   guild = Guild.find(guildmaster.current_guild_id)
+  #   render json: guild
+  # end
+  # ########
+  # ########for testing not for release
+  # ########
 
   # POST /guilds.json
   def create
-    guildmaster.build_guild
+    acc = Account.find(session[:account_id])
+    guildmaster = acc.guildmaster
+    if guildmaster.build_guild
+      result = {msg: :"success"}
+    else
+      result = {msg: :"error", detail: :"cannot_build_guild"}
+    end
+    render json: result.as_json
   end
 
 end
--- app/controllers/guildmaster_controller.rb
+++ app/controllers/guildmaster_controller.rb
@@ -2,19 +2,11 @@ class GuildmasterController < ApplicationController
   respond_to :json
   before_action :authorize
 
-  # GET /guildmaster.json
-
-  ########
-  ########for testing not for release
-  ########
+  # # GET /guildmaster.json
   def index
     acc = Account.find(session[:account_id])
     guildmaster = acc.guildmaster
     render json: guildmaster.as_json(except: [:created_at,:updated_at])
   end
-  ########
-  ########for testing not for release
-  ########
-
 end
 
