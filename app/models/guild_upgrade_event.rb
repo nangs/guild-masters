005c297d13c2d4845299f597b7786e63c1c81997
Hu Yifei <ascalon0823@gmail.com>
2016-03-30 17:55:06 +0800

    Implement upgrade event complete function

--- app/models/guild_upgrade_event.rb
+++ app/models/guild_upgrade_event.rb
@@ -1,4 +1,25 @@
 class GuildUpgradeEvent < ActiveRecord::Base
   belongs_to :guild
   delegate :guildmaster, to: :guild
+  
+  
+  def complete
+    guild = self.guild
+    guild.level = guild.level+1
+    gm =  self.guildmaster
+    gm.game_time = self.end_time
+    gm.state = "available"
+    facilities = guild.facilities
+    facMsg = Array.new
+    for fac in facilities
+      fac.capacity = guild.level*2
+      msg={facility: fac, capacity: fac.capacity}
+      facMsg<<msg
+      fac.save
+    end
+    guild.save
+    gm.save
+    completeMsg = {msg: :"success", guild_level: guild.level, adventurer_room: guild.level*5, quest_room: guild.level*10, facility_capacity: facMsg}
+    return completeMsg    
+  end
 end
