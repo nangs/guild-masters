550d41ae150807692d93da03536163dd44cd36ca
dtky <danieltangkengyan@gmail.com>
2016-04-01 16:29:18 +0800

    make use of database_cleaner to clean database before each testcases

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,4 +59,16 @@ RSpec.configure do |config|
   config.include(EmailSpec::Helpers)
   config.include(EmailSpec::Matchers)
   config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
 end
