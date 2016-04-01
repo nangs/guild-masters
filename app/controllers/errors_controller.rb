a6ec62cea72e668153ce3b90e8febf504231ed1c
dtky <danieltangkengyan@gmail.com>
2016-04-01 15:04:20 +0800

    edited error 404 page and restrict routes to used ones

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -4,6 +4,6 @@ class ErrorsController < ApplicationController
       format.html { render status: 404 }
     end
   rescue ActionController::UnknownFormat
-    render status: 404, text: "Error 404: Sorry, the url you have entered is not available"
+    render status: 404, text: "Error 404: Sorry, the url you have entered is not available\n\nPlease enter http://172.25.79.43:3000/ or http://127.0.0.1:3000/"
   end
 end
