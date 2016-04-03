76311eab907e278e382bdfb09a6e02088f9f90f0
dtky <danieltangkengyan@gmail.com>
2016-04-03 19:40:57 +0800

    add test cases for guildmaster_controller

--- app/controllers/guildmaster_controller.rb
+++ app/controllers/guildmaster_controller.rb
@@ -7,8 +7,13 @@ class GuildmasterController < ApplicationController
     acc = Account.find(session[:account_id])
     guildmaster = acc.guildmaster
     if params[:cmd] == "get"
-      render json: guildmaster.as_json(except: [:created_at,:updated_at])
+      result = {msg: "success", guildmaster: guildmaster}
+    elsif params[:cmd].nil?
+      result = {msg: :"error", detail: :"cmd_nil"}
+    else
+      result = {msg: :"error", detail: :"no_such_cmd"}
     end
+    render json: result.as_json(:except => [:created_at, :updated_at])
   end
 end
 
---
+++ spec/controllers/guildmaster_controller_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+#rspec spec/controllers/guildmaster_controller_spec.rb
+#zeus test spec/controllers/guildmaster_controller_spec.rb
+
+RSpec.describe GuildmasterController do
+  before :each do
+    @activated_account = create(:account, :activated)
+    @guildmaster = @activated_account.guildmaster
+    @guild = Guild.find_by(guildmaster_id: @guildmaster.id)
+    @guildmaster.current_guild_id = @guild.id
+    @guildmaster.save
+  end
+  describe 'POST #create' do
+    context "valid session" do
+      context "when params[:cmd] == get" do
+        it "get guildmaster info" do
+          request.session[:account_id] = @activated_account.id
+          post :create, {cmd: "get"} , format: :json
+          expect(response.status).to eq(200)
+          expect(Account.count).to eq(1)
+          expect(Guild.count).to eq(1)
+          expect(ActiveSupport::JSON.decode(response.body)).not_to be_nil
+          expect(session[:account_id]).to_not be nil
+          @msg_expected = "success"
+          parsed_body = JSON.parse(response.body)
+          expect(parsed_body["msg"]).to eq(@msg_expected)
+          expect(parsed_body["guildmaster"]).to_not be nil
+        end
+      end
+      context "when params[:cmd] == nil or not valid" do
+        context "invalid params" do
+          it "empty cmd" do
+            request.session[:account_id] = @activated_account.id
+            post :create, {cmd: nil} , format: :json
+            expect(response.status).to eq 200
+            expect(Account.count).to eq(1)
+            expect(ActiveSupport::JSON.decode(response.body)).not_to be_nil
+            @msg_expected = "error"
+            @detail_expected = "cmd_nil"
+            parsed_body = JSON.parse(response.body)
+            expect(parsed_body["msg"]).to eq(@msg_expected)
+            expect(parsed_body["detail"]).to eq(@detail_expected)
+          end
+        end
+        context "error" do
+          it "invalid cmd" do
+            request.session[:account_id] = @activated_account.id
+            post :create, {cmd: "not_valid_cmd"}, format: :json
+            expect(response.status).to eq 200
+            expect(Account.count).to eq(1)
+            expect(ActiveSupport::JSON.decode(response.body)).not_to be_nil
+            @msg_expected = "error"
+            @detail_expected = "no_such_cmd"
+            parsed_body = JSON.parse(response.body)
+            expect(parsed_body["msg"]).to eq(@msg_expected)
+            expect(parsed_body["detail"]).to eq(@detail_expected)
+          end
+        end
+      end
+    end
+    context "invalid session" do
+      it "renders 401" do
+        post :create, {cmd: "create"}
+        expect(response.status).to eq(401)
+      end
+    end
+  end
+end
