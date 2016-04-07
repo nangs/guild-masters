d1e11ddea3c26dd6aaa3021ea86d2bccaae65ae4
He Buwei <hebuweitom@gmail.com>
2016-04-07 11:11:02 +0800

    require login for admin pages

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,7 +6,14 @@ class AdminController < ApplicationController
   respond_to :json
 
   def authorize
-
+  	acc = Account.find_by(id: session[:account_id])
+    if acc
+      if !acc.is_admin
+        redirect_to '/'
+      end
+    else
+      redirect_to '/'
+    end
   end
 
 end
