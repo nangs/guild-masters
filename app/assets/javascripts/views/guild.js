40873aa004f2e02b5e4b179f65eaa6e092b6051e
zhouyichen <zhouyichen123@gmail.com>
2016-04-07 09:12:41 +0800

    Implement template for displaying result of upgrade event

--- app/assets/javascripts/views/guild.js
+++ app/assets/javascripts/views/guild.js
@@ -1,3 +1,4 @@
 Handlebars.registerPartial('guild',  HandlebarsTemplates['guild/guild']);
 var guildTemplate = HandlebarsTemplates['guild/guild'];
 var guildUpgradingTemplate = HandlebarsTemplates['guild/guild_upgrading'];
+var upgradeResultTemplate = HandlebarsTemplates['guild/guild_upgrade_result'];
