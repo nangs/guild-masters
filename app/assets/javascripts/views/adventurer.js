71c9ea4d127c3a9a4796cf851b6470c3d1f5f7f4
zhouyichen <zhouyichen123@gmail.com>
2016-04-13 18:20:58 +0800

    Render adventure attributes as a star shape

--- app/assets/javascripts/views/adventurer.js
+++ app/assets/javascripts/views/adventurer.js
@@ -23,18 +23,19 @@ var adventurerDisplayTemplate = HandlebarsTemplates['adventurer/adventurer_displ
 function regularPolygonPoints(sideCount, radius){
     var sweep = Math.PI * 2 / sideCount;
     var points = [];
+    var angles = [];
     for (var i = 0; i < sideCount; i++){
     	var angle = i * sweep - Math.PI / 2;
+    	angles.push(angle);
         var x = radius * Math.cos(angle);
         var y = radius * Math.sin(angle);
         points.push({x: x,y: y});
     }
-    return points;
+    return [points, angles];
 }
 
 function renderAdventureDetails(adventurer) {
 	var canvas;
-	console.log(GM.renderDetailBox);
 	if (GM.renderDetailBox) {
 		canvas = GM.renderDetailBox;
 	} else {
@@ -44,16 +45,67 @@ function renderAdventureDetails(adventurer) {
 
 	var cx = 250;
 	var cy = 200;
-	var points=regularPolygonPoints(5, 200);
+	var radius = 200;
+	var points_angles = regularPolygonPoints(5, radius);
+	var points = points_angles[0];
+	var angles = points_angles[1]
+	var attributeNames = ['Max HP', 'Max Energy', 'Attack', 'Defense', 'Vision'];
+	var attributes = ['max_hp', 'max_energy', 'attack', 'defense', 'vision'];
 
-	var polygon = new fabric.Polygon(points, {
+
+	var border = new fabric.Polygon(points, {
 		left: cx,
 		top: cy,
 		stroke: 'red',
 		fill: 'white',
 		selectable: false
 	});
-	canvas.add(polygon);
+	canvas.add(border);
+
+
+	// Draw the text representing the attributes
+	for (var i = 0; i < 5; i++) {
+		var px = points[i].x + cx;
+		var py = points[i].y + cy;
+		var line = new fabric.Line([cx, cy, px, py], {
+			stroke: 'red',
+			selectable: false
+		});
+		canvas.add(line);
+		var text = new fabric.Text(attributeNames[i], {
+			left: px,
+			top: py,
+			fontSize: 14,
+			selectable: false
+		});
+		text.set('backgroundColor', 'white');
+		canvas.add(text);
+	}
+
+	var adventurer_points = [];
+	for (var i = 0; i < 5; i++) {
+		var attr = attributes[i];
+		var value = adventurer[attr];
+		var percentage;
+		if (attr != 'max_hp') {
+			percentage = value / GM.max_adventurer_attributes;
+		} else {
+			percentage = value / GM.max_adventurer_max_hp;
+		}
+		var distance = Math.sqrt(percentage) * radius;
+		var angle = angles[i];
+        var x = distance * Math.cos(angle);
+        var y = distance * Math.sin(angle);
+        adventurer_points.push({x: x ,y: y});
+	}
+
+	var attribute_polygon = new fabric.Polygon(adventurer_points, {
+		left: cx,
+		top: cy,
+		stroke: 'blue',
+		fill: 'rgba(0,0,0,0)',
+	});
+	canvas.add(attribute_polygon);
 
 	GM.renderDetailBox = canvas;
 	return canvas;
