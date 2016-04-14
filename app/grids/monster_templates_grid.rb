424fcb0f015161084810f1140548717a06f143f1
He Buwei <hebuweitom@gmail.com>
2016-04-14 10:39:03 +0800

    code cleaning

--- app/grids/monster_templates_grid.rb
+++ app/grids/monster_templates_grid.rb
@@ -1,5 +1,4 @@
 class MonsterTemplatesGrid
-
   include Datagrid
 
   scope do
@@ -7,24 +6,24 @@ class MonsterTemplatesGrid
   end
 
   filter(:name, :string) { |value| where("name like '%#{value}%'") }
-  filter(:max_hp, :integer, :range => true)
-  filter(:max_energy, :integer, :range => true)
-  filter(:attack, :integer, :range => true)
-  filter(:defense, :integer, :range => true)
-  filter(:invisibility, :integer, :range => true)
+  filter(:max_hp, :integer, range: true)
+  filter(:max_energy, :integer, range: true)
+  filter(:attack, :integer, range: true)
+  filter(:defense, :integer, range: true)
+  filter(:invisibility, :integer, range: true)
 
-  column(:name, :mandatory => true)
-  column(:max_hp, :mandatory => true)
-  column(:max_energy, :mandatory => true)
-  column(:attack, :mandatory => true)
-  column(:defense, :mandatory => true)
-  column(:invisibility, :mandatory => true)
+  column(:name, mandatory: true)
+  column(:max_hp, mandatory: true)
+  column(:max_energy, mandatory: true)
+  column(:attack, mandatory: true)
+  column(:defense, mandatory: true)
+  column(:invisibility, mandatory: true)
 
-  column(:actions, :html => true, :mandatory => true) do |monster_template|
-    link_to "Edit", edit_admin_monster_template_path(monster_template)
+  column(:actions, html: true, mandatory: true) do |monster_template|
+    link_to 'Edit', edit_admin_monster_template_path(monster_template)
   end
 
-  column(:actions, :html => true, :mandatory => true) do |monster_template|
-    link_to "Delete", admin_monster_template_path(monster_template), method: :delete, data: { confirm: 'Are you sure?' }
+  column(:actions, html: true, mandatory: true) do |monster_template|
+    link_to 'Delete', admin_monster_template_path(monster_template), method: :delete, data: { confirm: 'Are you sure?' }
   end
 end
