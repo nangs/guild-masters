51d36ed0fb9422176c39311c9407d3b6deb0810e
dtky <danieltangkengyan@gmail.com>
2016-04-15 17:09:49 +0800

    Revert "fixed Guild not found error when accessing guild"
    This reverts commit 8bbebf30b0ec5e4c2a3d8cecbbe4d47eaa449e37.

--- app/models/guildmaster.rb
+++ app/models/guildmaster.rb
@@ -14,7 +14,7 @@ class Guildmaster < ActiveRecord::Base
   def self.show_all
     all_guildmaster_details = []
     Account.where(is_admin: false).find_each do |user|
-      guild = user.guildmaster.Guild.first
+      guild = Guild.find_by(id: user.guildmaster.current_guild_id)
       all_guildmaster_details << {
         username: user.username,
         game_time: user.guildmaster.game_time,
@@ -44,6 +44,7 @@ class Guildmaster < ActiveRecord::Base
     canteen.capacity = 2
     canteen.save
     guild.save
+    self.current_guild_id = guild.id
     guild
   end
 
