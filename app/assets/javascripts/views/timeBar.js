8f83b4741642242581b1346189804dccdf70f36e
zhouyichen <zhouyichen123@gmail.com>
2016-04-16 16:52:27 +0800

    Refactor: code styling

--- app/assets/javascripts/views/timeBar.js
+++ app/assets/javascripts/views/timeBar.js
@@ -1,5 +1,5 @@
-var timeBar_start_point = 20;
-var timeBar_top_padding = 16;
+var TIMEBAR_LEFT_PADDING = 20;
+var TIMEBAR_TOP_PADDING = 16;
 function renderTimeBar(events, currentGameTime){
 	var canvas;
 	if (GM.timebar) {
@@ -8,14 +8,14 @@ function renderTimeBar(events, currentGameTime){
 		canvas = new fabric.Canvas('timeBar');
 	}
 	canvas.clear();
-	var timeBar = new fabric.Line([timeBar_start_point, timeBar_top_padding, 1100, timeBar_top_padding], {
+	var timeBar = new fabric.Line([TIMEBAR_LEFT_PADDING, TIMEBAR_TOP_PADDING, 1100, TIMEBAR_TOP_PADDING], {
 		strokeWidth: 10,
 		stroke: 'green'
 	});
 
 	var start = new fabric.Circle({
-		top : timeBar_top_padding,
-		left : timeBar_start_point,
+		top : TIMEBAR_TOP_PADDING,
+		left : TIMEBAR_LEFT_PADDING,
 		strokeWidth: 10,
 		radius: 10,
 		fill: '#fff',
@@ -23,8 +23,8 @@ function renderTimeBar(events, currentGameTime){
 	});
 
 	var nextDay = Math.floor(currentGameTime / 1000 + 1) * 1000;
-	var nextDayPoint = nextDay - currentGameTime + timeBar_start_point;
-	var endOfDay = new fabric.Line([nextDayPoint , timeBar_top_padding - 15, nextDayPoint, timeBar_top_padding + 15], {
+	var nextDayPoint = nextDay - currentGameTime + TIMEBAR_LEFT_PADDING;
+	var endOfDay = new fabric.Line([nextDayPoint , TIMEBAR_TOP_PADDING - 15, nextDayPoint, TIMEBAR_TOP_PADDING + 15], {
 		strokeWidth: 10,
 		stroke: 'red',
 		selectable: false
@@ -33,7 +33,7 @@ function renderTimeBar(events, currentGameTime){
 		'End of the\ncurrent day',
 		{
 			left: nextDayPoint,
-			top: timeBar_top_padding + 30,
+			top: TIMEBAR_TOP_PADDING + 30,
 			fontSize: 14,
 			stroke: 'rgb(200,100,200)'
 		}
@@ -87,8 +87,8 @@ function renderTimeBar(events, currentGameTime){
 function renderQuestMark(eve, currentGameTime, canvas) {
 	var endTime = eve.end_time - currentGameTime;
 	var circle = new fabric.Circle({
-		top : timeBar_top_padding,
-		left : endTime + timeBar_start_point,
+		top : TIMEBAR_TOP_PADDING,
+		left : endTime + TIMEBAR_LEFT_PADDING,
 		strokeWidth: 8,
 		radius: 8,
 		fill: '#fff',
@@ -100,8 +100,8 @@ function renderQuestMark(eve, currentGameTime, canvas) {
 		var quest = eve.quest;
 		var infoText = quest.description + "\nDifficulty: " + quest.difficulty + "\nReward :" + quest.reward;
 		var text = new fabric.Text(infoText, {
-			left: endTime + timeBar_start_point + 160,
-			top: timeBar_top_padding + 25,
+			left: endTime + TIMEBAR_LEFT_PADDING + 160,
+			top: TIMEBAR_TOP_PADDING + 25,
 			fontSize: 12,
 			stroke: 'rgb(200,100,50)'
 		});
@@ -124,8 +124,8 @@ function renderQuestMark(eve, currentGameTime, canvas) {
 function renderFacilityMark(eve, currentGameTime, canvas) {
 	var endTime = eve.end_time - currentGameTime;
 	var circle = new fabric.Circle({
-		top : timeBar_top_padding,
-		left : endTime + timeBar_start_point,
+		top : TIMEBAR_TOP_PADDING,
+		left : endTime + TIMEBAR_LEFT_PADDING,
 		strokeWidth: 8,
 		radius: 8,
 		fill: '#fff',
@@ -138,8 +138,8 @@ function renderFacilityMark(eve, currentGameTime, canvas) {
 		var adventurer = eve.adventurer.name;
 		var infoText = "Your adventurer\n" + adventurer +"\nis in the " + facility;
 		var text = new fabric.Text(infoText, {
-			left: endTime + timeBar_start_point + 45,
-			top: timeBar_top_padding + 25,
+			left: endTime + TIMEBAR_LEFT_PADDING + 45,
+			top: TIMEBAR_TOP_PADDING + 25,
 			fontSize: 12,
 			stroke: 'rgb(200,100,50)'
 		});
@@ -162,8 +162,8 @@ function renderFacilityMark(eve, currentGameTime, canvas) {
 function renderScoutMark(eve, currentGameTime, canvas) {
 	var endTime = eve.end_time - currentGameTime;
 	var circle = new fabric.Circle({
-		top : timeBar_top_padding,
-		left : endTime + timeBar_start_point,
+		top : TIMEBAR_TOP_PADDING,
+		left : endTime + TIMEBAR_LEFT_PADDING,
 		strokeWidth: 8,
 		radius: 8,
 		fill: '#fff',
@@ -174,8 +174,8 @@ function renderScoutMark(eve, currentGameTime, canvas) {
 	circle.showInfo = function () {
 		var infoText = "The GuildMaster is scouting\nfor Adventurers and Quests";
 		var text = new fabric.Text(infoText, {
-			left: endTime + timeBar_start_point + 70,
-			top: timeBar_top_padding + 15,
+			left: endTime + TIMEBAR_LEFT_PADDING + 70,
+			top: TIMEBAR_TOP_PADDING + 15,
 			fontSize: 12,
 			stroke: 'rgb(200,100,50)'
 		});
@@ -198,8 +198,8 @@ function renderScoutMark(eve, currentGameTime, canvas) {
 function renderUpgradeMark(eve, currentGameTime, canvas) {
 	var endTime = eve.end_time - currentGameTime;
 	var circle = new fabric.Circle({
-		top : timeBar_top_padding,
-		left : endTime + timeBar_start_point,
+		top : TIMEBAR_TOP_PADDING,
+		left : endTime + TIMEBAR_LEFT_PADDING,
 		strokeWidth: 8,
 		radius: 8,
 		fill: '#fff',
@@ -210,8 +210,8 @@ function renderUpgradeMark(eve, currentGameTime, canvas) {
 	circle.showInfo = function () {
 		var infoText = "The GuildMaster is\n upgrading the Guild.";
 		var text = new fabric.Text(infoText, {
-			left: endTime + timeBar_start_point + 50,
-			top: timeBar_top_padding + 15,
+			left: endTime + TIMEBAR_LEFT_PADDING + 50,
+			top: TIMEBAR_TOP_PADDING + 15,
 			fontSize: 12,
 			stroke: 'rgb(200,100,50)'
 		});
