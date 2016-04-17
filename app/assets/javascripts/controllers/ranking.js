66c92335f8e3563b770d676f697f8ad4fb9f4fb8
zhouyichen <zhouyichen123@gmail.com>
2016-04-17 10:46:29 +0800

    Sort players by level over game time

--- app/assets/javascripts/controllers/ranking.js
+++ app/assets/javascripts/controllers/ranking.js
@@ -1,6 +1,11 @@
 GM.RankingController = Ember.Controller.extend();
 
 GM.RankingController.displayRanking = function (players) {
+	players.sort(GM.RankingController.compareByLevelToTime);
 	var view = rankingTemplate({'users':players});
 	$('#gameWindow').html(view);
+}
+
+GM.RankingController.compareByLevelToTime = function (p1, p2) {
+	return p1.level / p1.game_time - p2.level / p2.game_time;
 }
