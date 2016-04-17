83a9a7d2eff4c92fca62a4e105dc782dfa0974b7
zhouyichen <zhouyichen123@gmail.com>
2016-04-17 15:03:03 +0800

    readable JSON for adventurer tests

--- spec/javascripts/adventurer_spec.js
+++ spec/javascripts/adventurer_spec.js
@@ -1,6 +1,97 @@
 describe("Testing frontend adventurer controller", function() {
-    var adventurersString = '[{"id":1,"hp":1178,"max_hp":1368,"energy":36,"max_energy":120,"attack":106,"defense":140,"vision":102,"state":"resting","guild_id":2,"name":"Pêro da Covilhã"},{"id":2,"hp":76,"max_hp":1263,"energy":132,"max_energy":132,"attack":150,"defense":127,"vision":125,"state":"available","guild_id":2,"name":"Matthew Flinders"},{"id":3,"hp":464,"max_hp":1053,"energy":0,"max_energy":112,"attack":116,"defense":102,"vision":102,"state":"available","guild_id":2,"name":"William Hovell"},{"id":4,"hp":1081,"max_hp":1081,"energy":65,"max_energy":128,"attack":124,"defense":108,"vision":123,"state":"available","guild_id":2,"name":"Yermak Timofeyevich"},{"id":5,"hp":0,"max_hp":1189,"energy":0,"max_energy":125,"attack":120,"defense":110,"vision":115,"state":"dead","guild_id":2,"name":"Francisco Pizarro"},{"id":6,"hp":2589,"max_hp":2589,"energy":127,"max_energy":127,"attack":228,"defense":195,"vision":152,"state":"available","guild_id":2,"name":"James Cook"},{"id":7,"hp":2592,"max_hp":2592,"energy":129,"max_energy":129,"attack":173,"defense":254,"vision":193,"state":"available","guild_id":2,"name":"Matthew Flinders"}]';
-    var adventurers = JSON.parse(adventurersString);
+    var adventurers = [
+        {
+            "id": 1,
+            "hp": 1178,
+            "max_hp": 1368,
+            "energy": 36,
+            "max_energy": 120,
+            "attack": 106,
+            "defense": 140,
+            "vision": 102,
+            "state": "resting",
+            "guild_id": 2,
+            "name": "Pêro da Covilhã"
+        },
+        {
+            "id": 2,
+            "hp": 76,
+            "max_hp": 1263,
+            "energy": 132,
+            "max_energy": 132,
+            "attack": 150,
+            "defense": 127,
+            "vision": 127,
+            "state": "assigned",
+            "guild_id": 2,
+            "name": "Matthew Flinders"
+        },
+        {
+            "id": 3,
+            "hp": 464,
+            "max_hp": 1053,
+            "energy": 0,
+            "max_energy": 112,
+            "attack": 116,
+            "defense": 102,
+            "vision": 102,
+            "state": "available",
+            "guild_id": 2,
+            "name": "William Hovell"
+        },
+        {
+            "id": 4,
+            "hp": 1081,
+            "max_hp": 1081,
+            "energy": 65,
+            "max_energy": 128,
+            "attack": 124,
+            "defense": 108,
+            "vision": 123,
+            "state": "available",
+            "guild_id": 2,
+            "name": "Yermak Timofeyevich"
+        },
+        {
+            "id": 5,
+            "hp": 0,
+            "max_hp": 1189,
+            "energy": 0,
+            "max_energy": 125,
+            "attack": 120,
+            "defense": 110,
+            "vision": 115,
+            "state": "dead",
+            "guild_id": 2,
+            "name": "Francisco Pizarro"
+        },
+        {
+            "id": 6,
+            "hp": 2589,
+            "max_hp": 2589,
+            "energy": 127,
+            "max_energy": 127,
+            "attack": 228,
+            "defense": 195,
+            "vision": 152,
+            "state": "available",
+            "guild_id": 2,
+            "name": "James Cook"
+        },
+        {
+            "id": 7,
+            "hp": 2592,
+            "max_hp": 2592,
+            "energy": 129,
+            "max_energy": 129,
+            "attack": 173,
+            "defense": 254,
+            "vision": 195,
+            "state": "assigned",
+            "guild_id": 2,
+            "name": "Matthew Flinders"
+        }
+    ];
     
     it("filter alive adventurers", function() {
         var filteredAdventurers = GM.AdventurerController.filterAlive(adventurers);
