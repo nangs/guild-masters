000fea3b926b646739c61ac03417135aeff2e494
zhouyichen <zhouyichen123@gmail.com>
2016-04-20 13:35:22 +0800

    Implement default ranking scheme

--- app/assets/javascripts/controllers/ranking.js
+++ app/assets/javascripts/controllers/ranking.js
@@ -1,12 +1,28 @@
 GM.RankingController = Ember.Controller.extend();
 
 GM.RankingController.displayRanking = function (players) {
-	players.sort(GM.RankingController.compareByLevelToTime);
+	players.sort(GM.RankingController.defaultRanking);
 	for (var i = 0; i < players.length; i++) {
 		players[i].ranking = i + 1;
 	}
 	var view = rankingTemplate({'users':players});
 	$('#gameWindow').html(view);
+}
+
+GM.RankingController.defaultRanking = function (p1, p2) {
+	if (p2.level != p1.level) {
+		return p2.level - p1.level;
+	} else {
+		if (p2.popularity != p1.popularity) {
+			return p2.popularity - p1.popularity;
+		} else {
+			if (p2.gold != p1.gold) {
+				return p2.gold - p1.gold;
+			} else {
+				return GM.RankingController.compareByLevelToTime(p1, p2);
+			}
+		}
+	}
 }
 
 GM.RankingController.compareByLevelToTime = function (p1, p2) {
