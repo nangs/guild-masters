a1de27277fad5aaf2d58a50fe9125ed8d3a63596
zhouyichen <zhouyichen123@gmail.com>
2016-04-20 13:22:28 +0800

    Fix ranking order

--- app/assets/javascripts/controllers/ranking.js
+++ app/assets/javascripts/controllers/ranking.js
@@ -10,13 +10,13 @@ GM.RankingController.displayRanking = function (players) {
 }
 
 GM.RankingController.compareByLevelToTime = function (p1, p2) {
-	return p1.level / p1.game_time - p2.level / p2.game_time;
+	return p2.level / p2.game_time - p1.level / p1.game_time;
 }
 
 GM.RankingController.compareByPopularityToTime = function (p1, p2) {
-	return p1.popularity / p1.game_time - p2.popularity / p2.game_time;
+	return p2.popularity / p2.game_time - p1.popularity / p1.game_time;
 }
 
 GM.RankingController.compareByGoldToTime = function (p1, p2) {
-	return p1.gold / p1.game_time - p2.gold / p2.game_time;
+	return p2.gold / p2.game_time - p1.gold / p1.game_time;
 }
