b0f6a44c1797a589961426ea1fd616d7fba15570
zhouyichen <zhouyichen123@gmail.com>
2016-04-21 14:14:00 +0800

    Refactor: extra the ajax calls from controller to model

--- app/assets/javascripts/controllers/index.js
+++ app/assets/javascripts/controllers/index.js
@@ -56,39 +56,7 @@ function setupLoginPage() {
         } 
         else {
         	submitted = true;
-            $.ajax({
-                type: 'POST',
-                url: 'sessions.json',
-                data: {
-                    email: email,
-                    password: password
-                },
-                success: function(feedback) {
-                	console.log(feedback);
-                    var msg = feedback.msg;
-                    if (msg == 'success') {
-                        var guild = feedback.guilds[0];
-                        GM.GuildModel.postGuildID(guild.id);
-                        sessionStorage.setItem('loggedIn', 1);
-                        showGame();
-                    } else {
-                        switch(feedback.detail) {
-                            case 'not_activated':
-                                GM.IndexController.showEmailNotActivated(email);
-                                break;
-                            case 'wrong_password':
-                                showWrongPasswordError();
-                                break;
-                            case 'invalid_account':
-                                showEmailNotValid();
-                                break;
-                            case 'unknown':
-                                showLoginError();
-                                break;
-                        }    
-                    }
-                }
-            });
+            GM.AccountModel.login(email, password);
         }
     });
     $('#forgetPassword').mouseup(function() {
@@ -139,38 +107,7 @@ function setupSignupPage(email, password) {
         }
         else {
             submitted = true;
-            $.ajax({
-                type: 'POST',
-                url: 'accounts.json',
-                data: {
-                	cmd: 'signup',
-                    email: email,
-                    username: username,
-                    password: password
-                },
-                success: function(feedback) {
-                	console.log(feedback);
-                    var msg = feedback.msg;
-                    if (msg == 'success') {
-                        showSuccessSignupPage(email);
-                    } else {
-                        switch(feedback.detail) {
-                            case 'account_taken':
-                                showEmailTaken();
-                                break;
-                            case 'username_taken':
-                                showUsernameTaken();
-                                break;
-                            case 'not_activated':
-                                GM.IndexController.showEmailNotActivated(email);
-                                break;
-                            case 'unknown':
-                                showSignupError();
-                                break;
-                        }                        
-                    }
-                }
-            });
+            GM.AccountModel.signup(email, username, password);
         }
     });
 }
@@ -210,33 +147,7 @@ function setupForgetPasswordPage(email, password) {
             showSignupNullError('email');
         }
         else {
-            $.ajax({
-                type: 'POST',
-                url: 'accounts.json',
-                data: {
-                    cmd: 'send_password_token',
-                    email: email
-                },
-                success: function(feedback) {
-                    console.log(feedback);
-                    var msg = feedback.msg;
-                    if (msg == 'success') {
-                        showAlertMessage("The confirmation token has been sent to your email.");
-                    } else {
-                        switch(feedback.detail) {
-                            case 'not_activated':
-                                GM.IndexController.showEmailNotActivated(email);
-                                break;
-                            case 'invalid_account':
-                                showEmailNotValid();
-                                break;
-                            case 'unknown':
-                                showSignupError();
-                                break;
-                        }
-                    }
-                }
-            });
+            GM.AccountModel.sendPasswordToken(email); 
         }
     });
     $('#resetPassword').mouseup(function() {
@@ -255,41 +166,8 @@ function setupForgetPasswordPage(email, password) {
             showSignupNullError('password');
         } else if (token == '') {
             showSignupNullError('Confirmation code');
-        }
-        
-        else {
-            $.ajax({
-                type: 'POST',
-                url: 'accounts.json',
-                data: {
-                    cmd: 'update_account',
-                    email: email,
-                    password: password,
-                    confirm_token: token
-                },
-                success: function(feedback) {
-                    console.log(feedback);
-                    var msg = feedback.msg;
-                    if (msg == 'success') {
-                        showSuccessChangePasswordPage();
-                    } else {
-                        switch(feedback.detail) {
-                            case 'wrong_token':
-                                showWrongToken();
-                                break;
-                            case 'not_activated':
-                                GM.IndexController.showEmailNotActivated(email);
-                                break;
-                            case 'invalid_account':
-                                showEmailNotValid();
-                                break;
-                            case 'unknown':
-                                showSignupError();
-                                break;
-                        }
-                    }
-                }
-            });
+        } else {
+            GM.AccountModel.updateAccount(email, password, token);
         }
     });
 }
@@ -314,69 +192,15 @@ function setupActivateAccountButton(email) {
         var email = $('#email').val();
         if (!code) {
             showAlertMessage("Please enter the activation code");
-        }
-        else {
-            $.ajax({
-                type: 'POST',
-                url: 'accounts.json',
-                data: {
-                    cmd: 'activate_account',
-                    confirm_token: code,
-                    email: email
-                },
-                success: function(feedback) {
-                    console.log(feedback);
-                    if (feedback.msg == 'success') {
-                        showSuccessActivatePage();
-                    }
-                    else {
-                        switch(feedback.detail) {
-                            case 'wrong_token':
-                                showWrongToken();
-                                break;
-                            case 'already_activated':
-                                showEmailAlreadyActivated();
-                                break;
-                            case 'invalid_account':
-                                showEmailNotValid();
-                                break;
-                        }
-                    }
-                }
-            });
+        } else {
+            GM.AccountModel.activateAccount(code, email);
         }
     });
 }
 
 function setupResendEmailButton(email) {
     $('#resendEmail').mouseup(function() {
-        $.ajax({
-            type: 'POST',
-            url: 'accounts.json',
-            data: {
-                cmd: 'resend_email',
-                email: email
-            },
-            success: function(feedback) {
-                console.log(feedback);
-                if (feedback.msg == 'success') {
-                    showAlertMessage("Another email has been sent to you.");
-                }
-                else {
-                    switch(feedback.detail) {
-                        case 'not_activated':
-                            GM.IndexController.showEmailNotActivated();
-                            break;
-                        case 'invalid_account':
-                            showEmailNotValid();
-                            break;
-                        case 'unknown':
-                            showUnknownError();
-                            break;
-                    }
-                }
-            }
-        });
+        GM.AccountModel.resendEmail(email);
     });
 }
 
@@ -388,15 +212,7 @@ function showSuccessActivatePage() {
 }
 
 function logout() {
-    $.ajax({
-        type: 'DELETE',
-        url: 'sessions.json',
-        success: function(feedback) {
-            console.log(feedback);
-            sessionStorage.removeItem('loggedIn');
-            location.reload();
-        }
-    });
+    GM.AccountModel.logout();
 }
 
 function showWrongPasswordError() {
